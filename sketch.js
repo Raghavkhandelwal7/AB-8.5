c53e835f6b5b9536166f41cac25847aff067930f
Raghavkhandelwal7 <raghav.khandelwal5114@gmail.com>
2020-11-17 15:24:05 +0530

    emptied trajectory

--- sketch.js
+++ sketch.js
@@ -89,7 +89,10 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-       // slingshot.attach(bird.body);
+        Matter.Body.setPosition(bird.body,{x:200,y:50});
+        slingshot.attach(bird.body);
+        bird.trajectory=[];
+        gameState="onSling";
     }
 }
 async function getBackgroundImage(){
